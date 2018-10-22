b63eb7b7f290a5eb6c8e5041a54297e5d58a4634
kaisha <kaishasatrio@match-advertising.com>
2018-10-22 15:52:59 +0700

    update tabel

--- admin/application/modules/templates_/views/adm_header.php
+++ admin/application/modules/templates_/views/adm_header.php
@@ -34,6 +34,7 @@
                   <li><a><i class="fa fa-newspaper-o"></i> Blog <span class="fa fa-chevron-down"></span></a>
                     <ul class="nav child_menu">
                       <li><a href="<?php echo base_url('blogpost')?>">Manage</a></li>
+                      <li><a href="<?php echo base_url('blogpost/categories')?>">Categories</a></li>
                     </ul>
                   </li>
                   <li><a><i class="fa fa-sliders"></i> Customize <span class="fa fa-chevron-down"></span></a>
