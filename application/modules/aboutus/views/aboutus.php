cf4127d9b8af9b0ec7fae33c0c89a3412b9c44ab
kai1313matchad <kaishasatrio@match-advertising.com>
2021-04-19 14:06:51 +0700

    update about us

--- application/modules/aboutus/views/aboutus.php
+++ application/modules/aboutus/views/aboutus.php
@@ -1,31 +1,39 @@
-<!-- Title Page -->
-	<section class="bg-title-page p-t-50 p-b-40 flex-col-c-m" style="background-image: url(<?php echo base_url()?>assets/frontend/images/heading-pages-02.jpg);">
+	<!-- Title Page -->
+	<section class="bg-title-page p-t-40 p-b-50 flex-col-c-m" style="background-image: url(<?= base_url()?>assets/frontend/images/misc/aboutus.jpg);">
 		<h2 class="l-text2 t-center">
-			Caption
+			About
 		</h2>
-		<p class="m-text13 t-center">
-			Sub Caption
-		</p>
 	</section>
 
-	<!-- Content page -->
-	<section class="bgwhite p-t-55 p-b-65">
-		
+	<!-- content page -->
+	<section class="bgwhite p-t-66 p-b-38">
 		<div class="container">
-
-	        <div class="row">
-
-	            <!-- Blog Entries Column -->
-	            <div class="col-md-8">
-	                <div class="row" id="aboutus_content">
+			<div class="row">
+				<div class="col-md-4 p-b-30">
+					<div class="hov-img-zoom">
+						<img src="<?= base_url()?>assets/frontend/images/misc/aboutus2.jpg" alt="IMG-ABOUT">
 					</div>
-	            </div>
+				</div>
+
+				<div class="col-md-8 p-b-30">
+					<h3 class="m-text26 p-t-15 p-b-16">
+						Our story
+					</h3>
+
+					<p class="p-b-28">
+						PT. Multi Artistikacithra Advertising yang dikenal dengan Match-Ad adalah biro iklan di Surabaya yang telah berdiri lebih dari 27 tahun, kami menyediakan layanan promosi terbaik untuk memenuhi kebutuhan klien untuk mengenalkan, mengkomunikasikan dan mempromosikan produknya ke pasar umum. Didirikan pada 22 Juni 1990 fokus segmen iklan media luar ruang terutama penyediaan lokasi strategis untuk memberikan layanan yang meningkatkan kualitas, kreativitas di setiap media promosi dan penggunaan bahan dan teknologi terkini.
+					</p>
+
+					<div class="bo13 p-l-29 m-l-9 p-b-10">
+						<p class="p-b-11">
+							Creativity is just connecting things. When you ask creative people how they did something, they feel a little guilty because they didn't really do it, they just saw something. It seemed obvious to them after a while.
+						</p>
+
+						<span class="s-text7">
+							- Steve Job’s
+						</span>
+					</div>
+				</div>
 			</div>
 		</div>
 	</section>
-	<!-- Container Selection -->
-	<div id="dropDownSelect1"></div>
-	<div id="dropDownSelect2"></div>
-	<div id="dropDownSelect3"></div>
-	<div id="dropDownSelect4"></div>
-	<div id="dropDownSelect5"></div>
