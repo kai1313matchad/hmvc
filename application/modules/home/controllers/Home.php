176161ded20f0e7da055a17c6a1c0d0e656a8dba
kai1313matchad <kaishasatrio@match-advertising.com>
2021-05-04 13:22:38 +0700

    update badge 040521

--- application/modules/home/controllers/Home.php
+++ application/modules/home/controllers/Home.php
@@ -39,24 +39,6 @@ class Home extends MX_Controller
       'BIB3578000008',
       'BIB3578000011',
       'BIB3578000013',
-
-      // 'BIB3572000001',
-      // 'BAL3514000001',
-      // 'BIB3574000001',
-      // 'BIB3574000002',
-      // 'BIB3512000002',
-      // 'BAL3509000001',
-      // 'BAL3509000003',
-      // 'BAL3509000002',
-      // 'BIB3174000001',
-      // 'BIB3578000020',
-      // 'BIB3578000011',
-      // 'BIB3578000024',
-      // 'BIB3578000008',
-      // 'BIB3578000019',
-      // 'BIB3578000021',
-      // 'BIB3578000022',
-      // 'BIB3515000002'
     );
     $html = '';
     foreach ($promoItems as $item) {
@@ -64,7 +46,7 @@ class Home extends MX_Controller
       $pic = $this->db->select('PRODPIC_PATH')->get_where('mona_prodpict', array('PROD_ID'=>$dt->PROD_ID))->row()->PRODPIC_PATH;
       $html .= '<div class="col-md-4 item-slick-promo">
                   <a href="'.base_url().'product/details/'.$dt->PROD_SLUG.'">
-                    <img src="'.base_url().'assets/frontend/images/misc/badgeimlek.png" class="notify-badge" alt="">
+                    <img src="'.base_url().'assets/frontend/images/misc/badge-lebaran.png" class="notify-badge" alt="">
                     <img src="'.base_url().'admin'.$pic.'" width="100%" class="img-promo" alt="'.$dt->PROD_NAME.'">
                   </a>
                   <div class="promo-title"><h5>'.$dt->PROD_NAME.'</h5></div>
